6eb58a207778828bb6c161eece871f731517d023
vasanthianne <vasanthi.anne@gmail.com>
2025-01-14 23:49:03 -0600

    oops

--- com/vasanthia/oops/IfElse.java
+++ com/vasanthia/oops/IfElse.java
@@ -3,7 +3,7 @@ package com.vasanthia.oops;
 public class IfElse {
     public static void main(String[] args)
     {
-        int age = 19;
+        int age = 18;
         if (age >=18)
         {
             System.out.println("You are an Adult");
